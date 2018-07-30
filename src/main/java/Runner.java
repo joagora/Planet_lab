71d188ab69e0f5d3f53fcab9f88aa08783a5f5d1
Joanna Gora <joanna.gora@protonmail.com>
2018-07-30 16:24:39 +0100

    finished mvp

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
         Planet planetMars = new Planet("Mars", 908973);
+        System.out.println(planetMars.getName() + " is " + planetMars.getSize());
+        System.out.println(planetMars.explodePlanet());
     }
 
 }
